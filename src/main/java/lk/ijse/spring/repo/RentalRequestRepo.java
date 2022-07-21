0d38a99b25dc94341d4ed5efcf03d847fd7752c5
parindyawijeseskara <parindyawijesekara@gmail.com>
2022-07-21 19:11:44 +0530

    Search Rental Request By Date Range

--- src/main/java/lk/ijse/spring/repo/RentalRequestRepo.java
+++ src/main/java/lk/ijse/spring/repo/RentalRequestRepo.java
@@ -2,11 +2,17 @@ package lk.ijse.spring.repo;
 
 import lk.ijse.spring.entity.RentalRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Date;
+import java.util.List;
 
 public interface RentalRequestRepo extends JpaRepository<RentalRequest,Integer> {
     RentalRequest findByRentalRequestId(Integer rentalRequestId);
 
+    @Query(value = "SELECT *\n" +
+            "FROM   RentalRequest\n" +
+            "WHERE  RentalRequest.pickUpDate BETWEEN ? AND ? OR RentalRequest.returnDate BETWEEN ? AND ?", nativeQuery = true)
+    List<RentalRequest> findAllByPickUpDateBetweenOrReturnDateBetween(String fromDate1,String toDate1,String fromDate2,String toDate2);
 
 }
