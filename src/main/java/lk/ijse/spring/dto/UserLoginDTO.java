5763b460213947827222ee4d74d8dd2540bebe91
parindyawijeseskara <parindyawijesekara@gmail.com>
2022-08-03 00:34:35 +0530

    Added UserLoginDTO

---
+++ src/main/java/lk/ijse/spring/dto/UserLoginDTO.java
@@ -0,0 +1,17 @@
+package lk.ijse.spring.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class UserLoginDTO {
+    private String userName;
+    private String password;
+    private Integer userTypeId;
+}
